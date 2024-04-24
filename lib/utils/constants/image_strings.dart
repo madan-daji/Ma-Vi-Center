315c8497f7726c1387a5fc74659d2c19a0ef1b58
The Dirt Hub <mb614348@gmail.com>
2024-04-24 22:38:46 +0545

    Constants imageStrings is completed

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+// This class contains all the App images in string Formats .
+class MaViImages {
+  // App Logos
+  static const String darkAppLogo = "assets/logos/schl_logo.png";
+  static const String lightAppLogo = "assets/logos/schl_logo.png";
+
+  // Social Logos
+  static const String google = "assets/icons/google_icon.png";
+  static const String facebook = "assets/icons/fb_icon.png";
+}
